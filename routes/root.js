7a73c6b7598825a8e60fa4c571eb34ebd7f8935a
TypeMania <112783160+TypeMania@users.noreply.github.com>
2023-05-06 14:04:07 -0700

    Final

--- routes/root.js
+++ routes/root.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const err = (req,res)=>{res.json({"error":"404 Not Found"})};
 
 router.get('/index(.html)?(/)?$|^/$', (req,res)=>{res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));});
-router.all('(?!/states)*', (req,res)=>{res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'))});
+router.get('^(?!/states)*$|*', (req,res)=>{res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'))});
+router.route('^(?!/states)*$|*') // See 5c(ii) of project requirements.
+  .post(err)
+  .patch(err)
+  .delete(err);
 
 module.exports = router;
